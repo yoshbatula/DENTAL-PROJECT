e6a7ecb255f5d905ea8c7adc86409c258557c0af
USER <meljuncenita@gmail.com>
2024-02-20 14:07:38 +0800

    NEW ADD APPOINTMENT WITH IMAGE

--- CCE107/src/main/java/org/example/cce107/MAINPAGECONT.java
+++ CCE107/src/main/java/org/example/cce107/MAINPAGECONT.java
@@ -7,7 +7,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+
 import javafx.stage.Stage;
 
 public class MAINPAGECONT {
@@ -32,38 +34,47 @@ public class MAINPAGECONT {
 
     @FXML
     private AnchorPane add_form;
+
+    @FXML
+    private AnchorPane addform_Main;
+
+    @FXML
+    private ImageView add_image;
+
     public void switchForm(ActionEvent event) {
+
 
         if (event.getSource() == homeBTN) {
             home_form.setVisible(true);
             home_form1.setVisible(true);
-            add_form.setVisible(false);
+            addform_Main.setVisible(false);
             doc_form.setVisible(false);
 
             home_form.setStyle("-fx-background-color: white");
             home_form1.setStyle("-fx-background-color: #D0E3FF");
-            add_form.setStyle("-fx-background-color: transparent");
+            addform_Main.setStyle("-fx-background-color: transparent");
             doc_form.setStyle("-fx-background-color: transparent");
 
         } else if (event.getSource() == addBTN) {
-            add_form.setVisible(true);
+            addform_Main.setVisible(true);
+            add_image.setVisible(true);
             home_form.setVisible(false);
             home_form1.setVisible(false);
             doc_form.setVisible(false);
 
-            add_form.setStyle("-fx-background-color: #f7f2eb");
+            addform_Main.setStyle("-fx-background-color: #F2F0DE");
             doc_form.setStyle("-fx-background-color: transparent");
             home_form1.setStyle("-fx-background-color: transparent");
             home_form.setStyle("-fx-background-color: transparent");
 
         } else if (event.getSource() == doctorBTN) {
             doc_form.setVisible(true);
-            add_form.setVisible(false);
+            addform_Main.setVisible(false);
             home_form1.setVisible(false);
             home_form.setVisible(false);
 
             doc_form.setStyle("-fx-background-color: #37A1C3");
-            add_form.setStyle("-fx-background-color: transparent");
+            addform_Main.setStyle("-fx-background-color: transparent");
             home_form1.setStyle("-fx-background-color: transparent");
             home_form.setStyle("-fx-background-color: transparent");
         }
